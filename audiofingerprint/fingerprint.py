f1550b27ad2854d04c3cb03b3acf132c004e9ad5
Fredrik Rose <fredrik.rose.developer@gmail.com>
2018-04-10 20:43:55 +0200

    Limit the time and frequency range for target zones

--- audiofingerprint/fingerprint.py
+++ audiofingerprint/fingerprint.py
@@ -15,7 +15,9 @@ FingerprintParameters = collect.namedtuple('FingerprintParameters',
                                             'window_overlap',
                                             'time_bin_size',
                                             'frequency_bin_size',
-                                            'target_zone_offset',
+                                            'target_zone_min_time_offset',
+                                            'target_zone_max_time_offset',
+                                            'target_zone_max_frequency_offset',
                                             'target_zone_size'])
 
 
@@ -28,7 +30,9 @@ def get_default_parameters():
                                  window_overlap=512,
                                  time_bin_size=1,
                                  frequency_bin_size=1,
-                                 target_zone_offset=16,
+                                 target_zone_min_time_offset=32,
+                                 target_zone_max_time_offset=128,
+                                 target_zone_max_frequency_offset=128,
                                  target_zone_size=8)
 
 
@@ -48,7 +52,10 @@ def fingerprint(signal, sampling_frequency, parameters: FingerprintParameters, v
     times = _quantize(feature_points[0], parameters.time_bin_size).tolist()
     frequencies = _quantize(feature_points[1], parameters.frequency_bin_size).tolist()
     fingerprints = _fingerprint_from_points(
-        times, frequencies, parameters.target_zone_offset, parameters.target_zone_size)
+        times, frequencies,
+        (parameters.target_zone_min_time_offset, parameters.target_zone_max_time_offset),
+        parameters.target_zone_max_frequency_offset,
+        parameters.target_zone_size)
     if visualize:
         visualizer.visualize_fingerprint(
             spectogram, time_axis, frequency_axis, times, frequencies, fingerprints, parameters)
@@ -81,35 +88,48 @@ def _quantize(values, bin_size):
     return values // bin_size
 
 
-def _fingerprint_from_points(times, frequencies, target_zone_offset, target_zone_size):
+def _fingerprint_from_points(times, frequencies, target_zone_time_offsets,
+                             target_zone_frequency_offset, target_zone_size):
     """
     Calculates fingerprints from time-frequency points.
     :param times: Time values of the points.
     :param frequencies: Frequency values of the points.
-    :param target_zone_offset: Offset (in number of points) from anchor point to the target zone.
-    :param target_zone_size: Number of points to relate each anchor point to.
+    :param target_zone_time_offsets: Target zone time offsets from anchor (min, max).
+    :param target_zone_frequency_offset: Target zone max frequency offset from anchor.
+    :param target_zone_size: Max number of points in a target zone.
     :return: Fingerprints, {descriptor: [times]}.
     """
-    def find_first_point_after_time(points, time):
+    def find_points_in_target_zone(anchor, points, time_offsets, max_frequency_offset, size):
         """
-        Finds the first point after a given time.
-        :param points: Time-frequency points.
-        :param time: Time.
-        :return: Index of first point after time or length of points if no point is found.
+        Finds the points in the target zone for a given anchor.
+        :param anchor: Anchor point.
+        :param points: Points.
+        :param time_offsets: Target zone time offsets from anchor (min, max).
+        :param max_time_offset: Target zone max time offset from anchor.
+        :param max_frequency_offset: Target zone max frequency offset from anchor.
+        :param size: Max number of points in a target zone.
+        :return: Points in target zone.
         """
-        for i, point in enumerate(points):
-            if point[0] > time:
-                return i
-        return len(points)
+        min_time = anchor[0] + time_offsets[0]
+        max_time = anchor[0] + time_offsets[1]
+        target_zone = []
+        for point in points:
+            if (min_time <= point[0] <= max_time
+                    and abs(anchor[1] - point[1]) <= max_frequency_offset):
+                target_zone.append(point)
+                if len(target_zone) >= size:
+                    break
+        return target_zone
 
     points = sorted(zip(times, frequencies), key=lambda point: (point[0], point[1]))
     fingerprints = collect.defaultdict(set)
     for anchor in points:
         anchor_time = anchor[0]
         anchor_frequency = anchor[1]
-        start_index = find_first_point_after_time(points, anchor_time + target_zone_offset)
-        target_zone = slice(start_index, start_index + target_zone_size)
-        for point in points[target_zone]:
+        target_zone = find_points_in_target_zone(
+            anchor, points, target_zone_time_offsets, target_zone_frequency_offset,
+            target_zone_size)
+        for point in target_zone:
             descriptor = _fingerprint_descriptor(point[0], point[1], anchor_frequency, anchor_time)
             fingerprints[descriptor].add(anchor_time)
     return dict(fingerprints)
