1989afef51715e76376eba6fea8a9e292f9b9a88
Fredrik Rose <fredrik.rose.developer@gmail.com>
2018-04-13 18:02:53 +0200

    Only handle valid audio file types
    Silently ignoring other file types.

--- server/teacher.py
+++ server/teacher.py
@@ -1,6 +1,7 @@
 """
 Audio teacher.
 """
+import datetime
 import os
 import subprocess as sp
 import time
@@ -11,6 +12,7 @@ from common import common
 from server import database as db
 
 
+_VALID_FILE_TYPES = ('.mp3', '.flac')
 _BUFFER_SIZE = 10**8
 
 
@@ -22,29 +24,34 @@ def teach(directory, database_path, clear_database=False, visualize=False):
     :param clear_database: Clear the database if true.
     :param visualize: Visualize fingerprint algorithm if true.
     """
-    number_of_files = _number_of_files_in_directory(directory)
+    number_of_files = _number_of_audio_files_in_directory(directory)
     database = db.FingerprintDatabase(database_path, clear_database)
     file_number = 1
+    total_start = time.time()
     for directory_path, _, file_names in os.walk(directory):
         for file in file_names:
-            title = os.path.splitext(file)[0]
-            print("Learning {} ({}/{})... ".format(title, file_number, number_of_files), end="", flush=True)
-            file_number += 1
-            start = time.time()
-            signal = _signal_from_mp3(os.path.join(directory_path, file), common.SAMPLING_FREQUENCY)
-            fingerprint = common.fingerprint(signal, visualize)
-            database.add_song(title, fingerprint)
-            end = time.time()
-            print("Done, took {:.1f} seconds.".format(end - start))
+            if os.path.splitext(file)[1] in _VALID_FILE_TYPES:
+                title = os.path.splitext(file)[0]
+                print("Learning {} ({}/{})... ".format(title, file_number, number_of_files), end="", flush=True)
+                file_number += 1
+                start = time.time()
+                signal = _signal_from_mp3(os.path.join(directory_path, file), common.SAMPLING_FREQUENCY)
+                fingerprint = common.fingerprint(signal, visualize)
+                database.add_song(title, fingerprint)
+                end = time.time()
+                print("Done, took {:.1f} seconds.".format(end - start))
+    total_time = round(time.time() - total_start)
+    print("Learned {} songs, took {}. ".format(file_number - 1, str(datetime.timedelta(seconds=total_time))),
+          end="", flush=True)
 
 
-def _number_of_files_in_directory(directory):
+def _number_of_audio_files_in_directory(directory):
     """
-    Counts the number of files in a directory (recursively).
+    Counts the number of audio files in a directory (recursively).
     :param directory: Path to directory.
     :return: Number of files in directory.
     """
-    return sum(len(files) for _, _, files in os.walk(directory))
+    return sum(sum(os.path.splitext(f)[1] in _VALID_FILE_TYPES for f in files) for _, _, files in os.walk(directory))
 
 
 def _signal_from_mp3(path, sampling_frequency):
